af78baddf458567d113c4640b6147685fdcd8519
Matheus Braynner <m.braynner@outlook.com>
2022-07-25 17:02:13 -0300

    ms-audit initiated

---
+++ MS-audit/src/main/java/com/compass/msclaudit/MsAuditApplication.java
@@ -0,0 +1,13 @@
+package com.compass.msclaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuditApplication.class, args);
+	}
+
+}
---
+++ MS-audit/src/test/java/com/compass/msclaudit/MsAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.msclaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
